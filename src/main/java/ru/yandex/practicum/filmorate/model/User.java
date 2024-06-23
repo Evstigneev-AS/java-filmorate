3029cf00451132f34f83ef99d9e3abd0764d4dab
Anatolii <Steg92sm13@yandex.ru>
2024-06-23 23:07:53 +0800

    сделал спринт 10 вроде сделал репозиторий публичным

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * User.
+ */
+@Data
+@Builder(toBuilder = true)
+public class User {
+    private Long id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+}
